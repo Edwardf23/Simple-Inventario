53a3af8642ac4c43f45f0c893bd5a7b3fb7e11ad
Laboratorio 109 Pc-07 <lab-109-inf-07>
2023-09-21 18:59:00 -0400

    first commit

---
+++ src/SimpleInventario.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.scanner;
+public class SimpleInventario {
+
+    public static int agregararticulo(){
+        Scanner scaner = new scanner(System.in);
+    }
+
+}
